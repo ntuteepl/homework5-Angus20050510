a47eb042e71518150ba7d548f7eb207918ff1361
Angus20050510 <145432971+Angus20050510@users.noreply.github.com>
2023-10-31 14:37:50 +0800

    Update hw5-4.c

--- hw5-4.c
+++ hw5-4.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    int h,m;
+    scanf("%d%d",&h,&m);
+    float ha=(float)h*30+0.5*m;
+    float ma=(float)m*6;
+    float d= ha-ma;
+    if(d<0){
+        d=-d;
+        printf("%.3f",d);
+    }
+    if(d>180){
+        d=360-d;
+        printf("%.3f",d);
+    }
+    else{
+        printf("%.3f",d);
+    }
+    return 0;
+}

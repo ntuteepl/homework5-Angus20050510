cb80386544eb6d213408c3a7e2bfe7d0800100ca
Angus20050510 <145432971+Angus20050510@users.noreply.github.com>
2023-10-31 13:55:20 +0800

    Update hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+    int i;
+    double m;
+    scanf("%d",&i);
+    if(i>0){
+        if(i>1500){
+            m=i*0.81*0.79;
+            printf("%.1lf",m);
+        }
+        else if(i<=1500 && i>800){
+            m=i*0.81;
+            printf("%.1lf",m);
+        }
+        else if(i<=800){
+            m=i*0.9;
+            printf("%.1lf",m);
+        }
+    }
+    return 0;
+}

92c2de6198e2480920e7a4c6602d23a1bc1b8e19
Angus20050510 <145432971+Angus20050510@users.noreply.github.com>
2023-10-31 13:40:45 +0800

    Update hw5-1.c

--- hw5-1.c
+++ hw5-1.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float tmpc;
+    double tmpf;
+    scanf("%f",&tmpc);
+    tmpf=tmpc*1.8+32;
+    printf("%.1lf\n",tmpf);
+    return 0;
+}
 
